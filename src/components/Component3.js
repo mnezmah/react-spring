c9adc7fe897f7545c08fd0c5e20d6d641c28a679
mnezmah <mario.nezmah@gmail.com>
2019-03-27 09:15:35 +0100

    added transitions

---
+++ src/components/Component3.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Component3() {
+  return (
+    <div style={c3Style}>
+        <h1>Component3</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis animi ipsum voluptatibus iusto consequuntur? Quisquam, aperiam? Nulla exercitationem dolore suscipit.</p>
+    </div>
+  )
+}
+
+const c3Style = {
+  background: 'skyblue',
+  color: 'white',
+  padding: '1.5rem 1.5rem 5rem 1.5rem'
+}
